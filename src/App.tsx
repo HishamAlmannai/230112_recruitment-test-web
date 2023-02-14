d6422d5106aeb5bc5093f0e58db1f974a5c6f756
Hisham Almannai <hisham@gmx.de>
2023-02-14 17:50:40 +0100

    Add useApiHook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import Map from "./Map"
+import { useApiGet, TApiResponse } from './useApiHook';
+
 
 export default function App() {
+
+  const data: TApiResponse = useApiGet(
+    'https://restcountries.com/v3.1/alpha?codes=de,fr,be,&fields=capital,latlng'
+  );
+  if (!data.loading) console.log(data);
+
+
   return (
     <div className="App">
-      <Map />
+         <Map />
     </div>
   )
 }
