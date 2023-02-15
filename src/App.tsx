4ff81721373336d0693cc10e7c1391f92900ead6
Hisham Almannai <hisham@gmx.de>
2023-02-15 16:18:34 +0100

    Add pick()

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Map from "./Map";
 import { useApiGet, TApiResponse } from "./useApiHook";
+import { pick } from "./pick";
 
 export default function App() {
   const apiData: TApiResponse = useApiGet(
     "https://restcountries.com/v3.1/alpha?codes=de,fr,be,&fields=capital,latlng"
   );
- // if (!apiData.loading) console.log(apiData.data);
+  const filteredData = apiData.data.map((Entry) =>
+    pick(Entry, "capital", "latlng")
+  );
+  if (!apiData.loading) console.log(filteredData);
 
   return (
     <div className="App">
---
+++ src/pick.ts
@@ -0,0 +1,9 @@
+export const pick = < T extends object, K extends keyof T >(
+    obj: T,
+    ...keys: K[]
+): Pick< T, K > =>
+    keys.reduce< any >( ( r, key ) => {
+        r[ key ] = obj[ key ];
+
+        return r;
+    }, {} );
